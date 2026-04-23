2e534834b475d51b701c714b2925bcec9ec52fca
p0s3id0n <apollo13686@gmail.com>
2026-04-23 20:38:15 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=AnyKernel3 by KernelSU Developers | Build by cctv18
+kernel.string=AnyKernel3 by KernelSU Developers | Build by @p0s3id0n
 do.devicecheck=0
 do.modules=0
 do.systemless=0
